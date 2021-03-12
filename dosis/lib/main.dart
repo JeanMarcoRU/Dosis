05511aa510acff6e27347e501b0eec41aac16d52
jeanma97 <jeanmarco1997@gmail.com>
2021-03-11 18:28:31 -0600

    Log in template

--- dosis/lib/main.dart
+++ dosis/lib/main.dart
@@ -1,89 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:dosis/Screens/Welcome/welcome_screen.dart';
+import 'package:dosis/constants.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Practica:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: SpeedDial(
-        backgroundColor: Colors.black,
-        closeManually: true,
-        child: Icon(Icons.adjust),
-        //animatedIcon: AnimatedIcons.list_view,
-        curve: Curves.bounceInOut,
-        children: [
-          SpeedDialChild(
-            onTap: _decrementCounter,
-            label: "Resta uno",
-            child: Icon(Icons.exposure_neg_1),
-            backgroundColor: Colors.red,
-          ),
-          SpeedDialChild(
-            onTap: _incrementCounter,
-            label: "Suma uno",
-            child: Icon(Icons.exposure_plus_1),
-            backgroundColor: Colors.green,
-          ),
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: WelcomeScreen(),
     );
   }
 }
