e1ac20f5041233bd7a9eff55536ae2d8f2911583
jeanma97 <jeanmarco1997@gmail.com>
2021-05-01 01:07:53 -0600

    log in listo
    falta log out y sing up

--- dosis/lib/Screens/Login/components/body.dart
+++ dosis/lib/Screens/Login/components/body.dart
@@ -1,25 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dosis/Classes/perfiles.dart';
-import 'package:dosis/Screens/UI/ui.dart';
+//import 'package:dosis/Screens/UI/ui.dart';
 import 'package:dosis/components/forgot_password.dart';
 import 'package:flutter/material.dart';
 import 'package:dosis/Screens/Login/components/background.dart';
 import 'package:dosis/Screens/Signup/signup_screen.dart';
 import 'package:dosis/components/already_have_an_account_acheck.dart';
 import 'package:dosis/components/rounded_button.dart';
-import 'package:dosis/components/rounded_input_field.dart';
-import 'package:dosis/components/rounded_password_field.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:encrypt/encrypt.dart' as EncryptAux;
+import 'package:dosis/authentication_service.dart';
+import 'package:provider/provider.dart';
+import 'package:dosis/components/text_field_container.dart';
 
 import '../../../constants.dart';
 
 class Body extends StatelessWidget {
-  const Body({
+  Body({
     Key key,
   }) : super(key: key);
 
   static List perfileslist = [];
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   static String user, password;
 
   @override
@@ -37,17 +41,34 @@ class Body extends StatelessWidget {
               height: size.height * 0.35,
             ),
             SizedBox(height: size.height * 0.05),
-            RoundedInputField(
-              hintText: "Email",
-              icon: Icons.mail,
-              onChanged: (value) {
-                user = value;
-              },
+            TextFieldContainer(
+              child: TextField(
+                controller: emailController,
+                cursorColor: kPrimaryColor,
+                decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.mail,
+                    color: kPrimaryColor,
+                  ),
+                  hintText: "Email",
+                  border: InputBorder.none,
+                ),
+              ),
             ),
-            RoundedPasswordField(
-              onChanged: (value) {
-                password = value;
-              },
+            TextFieldContainer(
+              child: TextField(
+                controller: passwordController,
+                obscureText: true,
+                cursorColor: kPrimaryColor,
+                decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.lock,
+                    color: kPrimaryColor,
+                  ),
+                  hintText: "Contraseña",
+                  border: InputBorder.none,
+                ),
+              ),
             ),
             ForgotPassword(
               press: () {
@@ -66,16 +87,12 @@ class Body extends StatelessWidget {
               text: "Iniciar sesión",
               color: kPrimaryLightColor,
               press: () {
-                encrypted(password);
+                context.read<AuthenticationService>().signIn(
+                      email: emailController.text.trim(),
+                      password: passwordController.text.trim(),
+                    );
+
                 cargaPerfiles();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return ui();
-                    },
-                  ),
-                );
               },
             ),
             SizedBox(height: size.height * 0.00),
@@ -124,22 +141,6 @@ class Body extends StatelessWidget {
           color: getColor(perfileslist[i]["color"])));
     }
   } //void
-
-  String encrypted(String texto) {
-    final plainText = texto;
-    final key = EncryptAux.Key.fromUtf8('my32lengthsupersecretnooneknows5');
-    final iv = EncryptAux.IV.fromLength(16);
-
-    final encrypter = EncryptAux.Encrypter(EncryptAux.AES(key));
-
-    final encrypted = encrypter.encrypt(plainText, iv: iv);
-    final decrypted = encrypter.decrypt(encrypted, iv: iv);
-
-    print(decrypted); // Lorem ipsum dolor sit amet, consectetur adipiscing elit
-    print(encrypted
-        .base64); // R4PxiU3h8YoIRqVowBXm36ZcCeNeZ4s1OvVBTfFlZRdmohQqOpPQqD1YecJeZMAop/hZ4OxqgC1WtwvX/hP9mw==
-    return encrypted.base64;
-  }
 
   Color getColor(String color) {
     switch (color) {
--- dosis/lib/main.dart
+++ dosis/lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:dosis/Screens/Welcome/welcome_screen.dart';
 import 'package:dosis/constants.dart';
+import 'package:dosis/Screens/UI/ui.dart';
+import 'package:provider/provider.dart';
+import 'Screens/Login/login_screen.dart';
+import 'authentication_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp().then((value) {
+  await Firebase.initializeApp().then((value) {
     runApp(MyApp());
   });
 }
@@ -14,14 +19,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Dosis',
-      theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        //scaffoldBackgroundColor: Colors.white,
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        // ignore: missing_required_param
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Dosis',
+        theme: ThemeData(
+          primaryColor: kPrimaryColor,
+        ),
+        home: AuthenticationWrapper(),
       ),
-      home: WelcomeScreen(),
     );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return ui();
+    }
+    return LoginScreen();
   }
 }
