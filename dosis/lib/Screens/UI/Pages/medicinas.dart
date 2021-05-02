25ad3afcfd6b4c79456d8266e13081bc449bfd25
Yanina <ycontreras@estudiantec.cr>
2021-05-02 09:39:16 -0600

    Cargar Categorias y medicamentos desde Firestore

--- dosis/lib/Screens/UI/Pages/medicinas.dart
+++ dosis/lib/Screens/UI/Pages/medicinas.dart
@@ -1,12 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dosis/Classes/categoria.dart';
+import 'package:dosis/Classes/medicamento.dart';
 import 'package:flutter/material.dart';
 import '../../../constants.dart';
 import 'Medicamentos/formCategoria.dart';
 import 'Medicamentos/formMedicamentos.dart';
 
 class Medicinas extends StatelessWidget {
-  const Medicinas({Key key}) : super(key: key);
+  Medicinas({Key key}) : super(key: key);
 
   static const String _title = 'Medicamentos';
+  final List categoriaslist = [];
+  final List medicamentosList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +20,59 @@ class Medicinas extends StatelessWidget {
       title: _title,
       home: BotonFlotante(),
     );
+  }
+
+  /* Funcion para cargar los datos de Categoria */
+  void cargaCategoria() async {
+    categoriaslist.clear();
+    categorias.clear();
+    CollectionReference collectionReference =
+        FirebaseFirestore.instance.collection("Categoria");
+
+    QuerySnapshot categoriaQS = await collectionReference.get();
+
+    if (categoriaQS.docs.length != 0) {
+      for (var doc in categoriaQS.docs) {
+        categoriaslist.add(doc.data());
+      }
+    }
+    categorias.clear();
+    for (var i = 0; i < categoriaslist.length; i++) {
+      categorias.add(Categoria(
+          letralogo: categoriaslist[i]["letralogo"],
+          nombre: categoriaslist[i]["Nombre"],
+          descripcion: categoriaslist[i]["Descripción"],
+          emoji: categoriaslist[i]["Emoji"]));
+    }
+    print(categorias);
+  }
+
+  /* Funcion para cargar los datos de los medicamentos */
+  void cargaMedicamentos() async {
+    medicamentosList.clear();
+    medicamentos.clear();
+    CollectionReference collectionReference =
+        FirebaseFirestore.instance.collection("Medicamento");
+
+    QuerySnapshot medicamentosQS = await collectionReference.get();
+
+    if (medicamentosQS.docs.length != 0) {
+      for (var doc in medicamentosQS.docs) {
+        medicamentosList.add(doc.data());
+      }
+    }
+    medicamentos.clear();
+    for (var i = 0; i < categoriaslist.length; i++) {
+      medicamentos.add(Medicamento(
+          letralogo: medicamentosList[i]["letralogo"],
+          nombre: medicamentosList[i]["Nombre"],
+          dosis: medicamentosList[i]["Dosis"],
+          tomaDesde: medicamentosList[i]["Periodo de Toma Desde"],
+          tomaHasta: medicamentosList[i]["Periodo de Toma Hasta"],
+          dias: medicamentosList[i]["Dias"],
+          hora: medicamentosList[i]["Hora"]));
+    }
+    print(medicamentos);
   }
 }
 
