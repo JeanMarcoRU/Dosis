395d5c823da252e8f585c0f38f3b65760da594e3
jeanma97 <jeanmarco1997@gmail.com>
2021-06-09 22:23:31 -0600

    calendar template

--- dosis/lib/Screens/UI/Pages/calendario.dart
+++ dosis/lib/Screens/UI/Pages/calendario.dart
@@ -1,9 +1,253 @@
 import 'package:dosis/Classes/perfiles.dart';
-import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../../../constants.dart';
 import 'calendario/mes_appbar_constructor.dart';
 import 'calendario/perfil_appbar_constructor.dart';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Calendario extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: calendario(),
+    );
+  }
+}
+
+class calendario extends StatefulWidget {
+  @override
+  _calendarioState createState() => _calendarioState();
+}
+
+class _calendarioState extends State<calendario> {
+  CalendarController _calendarController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _calendarController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple, //-------------------
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Select Date",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            calendarController: _calendarController,
+            initialCalendarFormat: CalendarFormat.week,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            formatAnimation: FormatAnimation.slide,
+            headerStyle: HeaderStyle(
+              centerHeaderTitle: true,
+              formatButtonVisible: false,
+              titleTextStyle: TextStyle(color: Colors.white, fontSize: 16),
+              leftChevronIcon: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
+                size: 15,
+              ),
+              rightChevronIcon: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+                size: 15,
+              ),
+              leftChevronMargin: EdgeInsets.only(left: 70),
+              rightChevronMargin: EdgeInsets.only(right: 70),
+            ),
+            calendarStyle: CalendarStyle(
+                weekendStyle: TextStyle(color: Colors.white),
+                weekdayStyle: TextStyle(color: Colors.white)),
+            daysOfWeekStyle: DaysOfWeekStyle(
+                weekendStyle: TextStyle(color: Colors.white),
+                weekdayStyle: TextStyle(color: Colors.white)),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40)),
+                  color: Colors.white),
+              child: Container(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "18 April 2020",
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Column(
+                        children: [
+                          dayTask("10 am", "Michael Hamilton"),
+                          dayTask("11 am", "Alexandra Johnson"),
+                          dayTask("2 pm", "Michael Hamilton"),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Row dayTask(String time, String name) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          width: MediaQuery.of(context).size.width * 0.2,
+          child: Text(
+            time,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.right,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(bottom: 20),
+            padding: EdgeInsets.all(20),
+            color: Color(0xffdfdeff),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.w700), //-------
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Upkeep Cleaning',
+                  style: TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.timer,
+                      color: Colors.purple, //-----------
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "$time - 5 pm",
+                      style: TextStyle(
+                          color: Colors.purple, //-----------
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Client Rating",
+                      style: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Icon(Icons.star),
+                    Icon(Icons.star),
+                    Icon(Icons.star),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  height: 0.5,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.call,
+                      color: Colors.purple, //--------
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Icon(
+                      Icons.mail_outline,
+                      color: Colors.purple, //--------------------------
+                    ),
+                    Expanded(
+                      child: Container(),
+                    ),
+                    Text(
+                      "\$50",
+                      style: TextStyle(
+                          color: Colors.purple, //-------------------
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+/*
 class Calendario extends StatefulWidget {
   const Calendario({Key key}) : super(key: key);
 
@@ -27,6 +271,7 @@ class _CalendarioState extends State<Calendario> {
     "noviembre",
     "diciembre",
   ];
+  List<int> dias = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -126,8 +371,9 @@ class _CalendarioState extends State<Calendario> {
       ),
     );
   }*/
-} //class Calendario
-
+} //class Calendario -----------------------------------------------------------
+*/
+//----------------------------------------------------------------------
 /*
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
