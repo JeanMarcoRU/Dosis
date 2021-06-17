e8d68bf47c411daf7eb92916730c457c5fc3f447
jeanma97 <jeanmarco1997@gmail.com>
2021-06-16 18:25:55 -0600

    hot reload en horario

--- dosis/lib/Screens/UI/Pages/calendario.dart
+++ dosis/lib/Screens/UI/Pages/calendario.dart
@@ -10,6 +10,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+// ignore: must_be_immutable
 class Calendario extends StatefulWidget {
   @override
   _CalendarioState createState() => _CalendarioState();
@@ -17,6 +18,7 @@ class Calendario extends StatefulWidget {
 
 class _CalendarioState extends State<Calendario> {
   List medicamentosFiltros = [];
+
   void queryMedicamentos() async {
     medicamentosFiltros.clear();
 
@@ -51,6 +53,7 @@ class _CalendarioState extends State<Calendario> {
   @override
   Widget build(BuildContext context) {
     queryMedicamentos();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: calendario(this.medicamentosFiltros),
@@ -61,6 +64,7 @@ class _CalendarioState extends State<Calendario> {
   }
 }
 
+// ignore: camel_case_types
 class calendario extends StatefulWidget {
   calendario(List medicamentosFiltros);
 
@@ -68,10 +72,12 @@ class calendario extends StatefulWidget {
   _calendarioState createState() => _calendarioState();
 }
 
+// ignore: camel_case_types
 class _calendarioState extends State<calendario> {
   CalendarController _calendarController;
   bool valuefirst = false;
   List medicamentosFiltros = [];
+  Column muestraMediBoxes;
   var fechaSeleccionada = DateTime.now();
   var fechaFormat = DateFormat('MM/dd/yyyy');
   @override
@@ -91,6 +97,16 @@ class _calendarioState extends State<calendario> {
   @override
   Widget build(BuildContext context) {
     queryMedicamentos();
+    muestraMediBoxes = Column(
+      children: medicamentosFiltros.map((medicamento) {
+        return MedicamentoBox(
+          userColor: medicamento.color,
+          nombre: medicamento.nombre,
+          hora: medicamento.hora,
+          dosis: medicamento.dosis,
+        );
+      }).toList(),
+    );
     return Scaffold(
       backgroundColor: Colors.white, //-------------------
       body: Column(
@@ -172,18 +188,7 @@ class _calendarioState extends State<calendario> {
                       ),
                       Column(
                         children: [
-                          Column(
-                            //scrollDirection: Axis.vertical,
-                            //shrinkWrap: true,
-                            children: medicamentosFiltros.map((medicamento) {
-                              return MedicamentoBox(
-                                userColor: medicamento.color,
-                                nombre: medicamento.nombre,
-                                hora: medicamento.hora,
-                                dosis: medicamento.dosis,
-                              );
-                            }).toList(),
-                          ),
+                          muestraMediBoxes,
                         ],
                       )
                     ],
@@ -401,322 +406,54 @@ class _calendarioState extends State<calendario> {
         scrollDirection: Axis.horizontal,
         shrinkWrap: true,
         children: perfiles.map((perfil) {
-          return PerfilAppbar(
-            perfil: perfil,
-            isPressed: perfil.visibilidad,
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
-
-/*
-class Calendario extends StatefulWidget {
-  const Calendario({Key key}) : super(key: key);
-
-  @override
-  _CalendarioState createState() => _CalendarioState();
-}
-
-class _CalendarioState extends State<Calendario> {
-  //bool _isPressed = true;
-  List<String> meses = [
-    "enero",
-    "febrero",
-    "marzo",
-    "abril",
-    "mayo",
-    "junio",
-    "julio",
-    "agosto",
-    "septiembre",
-    "octubre",
-    "noviembre",
-    "diciembre",
-  ];
-  List<int> dias = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[
-          _perfiles(),
-          _meses(),
-        ],
-      ),
-    );
-  }
-
-  Widget _perfiles() {
-    return Container(
-      height: 90,
-      width: double.infinity,
-      margin: EdgeInsets.only(top: 45),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          bottom: BorderSide(width: 1.0, color: kgreyLColor),
-        ),
-      ),
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        children: perfiles.map((perfil) {
-          return PerfilAppbar(
-            perfil: perfil,
-            isPressed: true,
-          );
-        }).toList(),
-      ),
-    );
-  }
-
-  Widget _meses() {
-    return Container(
-      height: 40,
-      width: double.infinity,
-      margin: EdgeInsets.only(top: 10),
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        children: meses.map((mes) {
-          return MesAppbar(
-            mes: mes,
-            isPressed: false,
-          );
-        }).toList(),
-      ),
-    );
-  }
-
-  /*
-  Widget _crearPerfilesAppBar(Perfil perfil) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _isPressed = !_isPressed;
-        });
-      },
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(5),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Container(
-                padding: EdgeInsets.all(0),
-                height: 55,
-                width: 55,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(100),
-                  border: Border.all(
-                    width: 1,
-                    color: perfil.color,
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    perfil.letralogo,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: _isPressed ? perfil.color : Colors.amber,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 35,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }*/
-} //class Calendario -----------------------------------------------------------
-*/
-//----------------------------------------------------------------------
-/*
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  DateTime _currentDate = DateTime(2020, 8, 3);
-  DateTime _currentDate2 = DateTime(2020, 8, 4);
-  String _currentMonth = DateFormat.yMMM().format(DateTime(2020, 8, 3));
-  DateTime _targetDateTime = DateTime(2020, 8, 3);
-
-  static Widget _eventIcon = new Container(
-    decoration: new BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(1000)),
-        border: Border.all(color: Colors.green, width: 2.0)),
-    child: new Icon(
-      Icons.person,
-      color: Colors.amber,
-    ),
-  );
-
-  EventList<Event> _markedDateMap = new EventList<Event>(
-    events: {
-      new DateTime(2020, 8, 5): [
-        new Event(
-          date: new DateTime(2020, 8, 5),
-          title: 'Event 1',
-          icon: _eventIcon,
-          dot: Container(
-            margin: EdgeInsets.symmetric(horizontal: 1.0),
-            color: Colors.green,
-            height: 5.0,
-            width: 5.0,
-          ),
-        ),
-        new Event(
-          date: new DateTime(2020, 8, 5),
-          title: 'Event 2',
-          icon: _eventIcon,
-        ),
-      ],
-    },
-  );
-  CalendarCarousel _calendarCarousel, _calendarCarouselNoHeader;
-  @override
-  void initState() {
-    _markedDateMap.addAll(new DateTime(2020, 8, 13), [
-      new Event(
-        date: new DateTime(2020, 8, 13),
-        title: 'Event 1',
-        icon: _eventIcon,
-      ),
-      new Event(
-        date: new DateTime(2020, 8, 13),
-        title: 'Event 2',
-        icon: _eventIcon,
-      ),
-      new Event(
-        date: new DateTime(2020, 8, 13),
-        title: 'Event 3',
-        icon: _eventIcon,
-      ),
-      new Event(
-        date: new DateTime(2020, 8, 13),
-        title: 'Event 3',
-        icon: _eventIcon,
-      ),
-    ]);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    _calendarCarouselNoHeader = CalendarCarousel<Event>(
-      todayBorderColor: Colors.green,
-      onDayPressed: (DateTime date, List<Event> events) {
-        this.setState(() => _currentDate2 = date);
-        events.forEach((event) => print(event.title));
-      },
-      daysHaveCircularBorder: true,
-      showOnlyCurrentMonthDate: true,
-      weekendTextStyle: TextStyle(
-        color: Colors.black,
-      ),
-      thisMonthDayBorderColor: Colors.grey,
-      weekFormat: false,
-//      firstDayOfWeek: 4,
-      markedDatesMap: _markedDateMap,
-      height: 420.0,
-      selectedDateTime: _currentDate2,
-      targetDateTime: _targetDateTime,
-      customGridViewPhysics: NeverScrollableScrollPhysics(),
-
-      minSelectedDate: _currentDate.subtract(Duration(days: 360)),
-      maxSelectedDate: _currentDate.add(Duration(days: 360)),
-
-      inactiveDaysTextStyle: TextStyle(
-        color: Colors.pink,
-        fontSize: 16,
-      ),
-      onCalendarChanged: (DateTime date) {
-        this.setState(() {
-          _targetDateTime = date;
-          _currentMonth = DateFormat.yMMM().format(_targetDateTime);
-        });
-      },
-      onDayLongPressed: (DateTime date) {
-        print('long pressed date $date');
-      },
-    );
-
-    return new Scaffold(
-        appBar: new AppBar(
-          backgroundColor: Colors.black,
-          title: new Text(widget.title),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              //custom icon
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 16.0),
-                child: _calendarCarousel,
-              ), // This trailing comma makes auto-formatting nicer for build methods.
-              //custom icon without header
-              Container(
-                margin: EdgeInsets.only(
-                  top: 30.0,
-                  bottom: 16.0,
-                  left: 16.0,
-                  right: 16.0,
-                ),
-                child: new Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: Text(
-                      _currentMonth,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24.0,
+          Perfil perfilAux;
+          bool isPressed;
+          perfilAux = perfil;
+          isPressed = perfilAux.visibilidad;
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                isPressed = !isPressed;
+                perfilAux.visibilidad = isPressed;
+              });
+            },
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(5),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Container(
+                      padding: EdgeInsets.all(0),
+                      height: 55,
+                      width: 55,
+                      decoration: BoxDecoration(
+                        color: isPressed ? perfilAux.color : Colors.white,
+                        borderRadius: BorderRadius.circular(100),
+                        border: Border.all(
+                          width: 1,
+                          color: perfilAux.color,
+                        ),
                       ),
-                    )),
-                    FlatButton(
-                      child: Text('PREV'),
-                      onPressed: () {
-                        setState(() {
-                          _targetDateTime = DateTime(
-                              _targetDateTime.year, _targetDateTime.month - 1);
-                          _currentMonth =
-                              DateFormat.yMMM().format(_targetDateTime);
-                        });
-                      },
+                      child: Center(
+                        child: Text(
+                          perfilAux.letralogo,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: isPressed ? Colors.white : perfilAux.color,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35,
+                          ),
+                        ),
+                      ),
                     ),
-                    FlatButton(
-                      child: Text('NEXT'),
-                      onPressed: () {
-                        setState(() {
-                          _targetDateTime = DateTime(
-                              _targetDateTime.year, _targetDateTime.month + 1);
-                          _currentMonth =
-                              DateFormat.yMMM().format(_targetDateTime);
-                        });
-                      },
-                    )
-                  ],
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 16.0),
-                child: _calendarCarouselNoHeader,
-              ), //
-            ],
-          ),
-        ));
+              ],
+            ),
+          );
+        }).toList(),
+      ),
+    );
   }
-}*/
+}
