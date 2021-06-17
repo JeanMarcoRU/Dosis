40284ebe262afbb61a9c7cc27c533189b6162214
jeanma97 <jeanmarco1997@gmail.com>
2021-06-16 20:02:43 -0600

    hot reload medicamentos

--- dosis/lib/Screens/UI/Pages/medicinas.dart
+++ dosis/lib/Screens/UI/Pages/medicinas.dart
@@ -221,19 +221,122 @@ class _MedicinasState extends State<Medicinas> {
   }
 
   Widget _medicamentos() {
+    String emoji = "⚕️";
     queryMedicamentos();
     return ListView.builder(
       padding: EdgeInsets.all(8.0),
       itemCount: medicamentosFiltro.length,
-      itemBuilder: (context, index) => MedicamentoObj(
-        i: index,
-        medicamento: medicamentosFiltro[index],
-        press: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => detailsMedicamento(
-                      medicamento: medicamentosFiltro[index],
-                    ))),
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => detailsMedicamento(
+              medicamento: medicamentosFiltro[index],
+            ),
+          ),
+        ),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 10, bottom: 0, left: 10, right: 10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  padding: EdgeInsets.all(15),
+                  height: 80,
+                  //width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    color: medicamentosFiltro[index].color,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              /*image: const DecorationImage(
+                          image: NetworkImage(
+                              'https://ichef.bbci.co.uk/news/640/cpsprodpb/1DD7/production/_102793670_1.jpg'),
+                          fit: BoxFit.cover,
+                        ),*/
+                            ),
+                            /*child: Icon(
+                        Icons.beach_access,
+                        color: Colors.blue,
+                        size: 36.0,
+                      ),*/
+                            child: Center(
+                              child: Text(
+                                emoji,
+                                style: TextStyle(
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 7,
+                      ),
+                      Container(
+                        width: 200,
+                        //color: Colors.black,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              medicamentosFiltro[index].nombre,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(
+                              medicamentosFiltro[index].dosis,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w200,
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(
+                              medicamentosFiltro[index].hora,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w200,
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios_sharp,
+                        color: Colors.white,
+                        size: 45,
+                        //semanticLabel: 'Text to announce in accessibility modes',
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
